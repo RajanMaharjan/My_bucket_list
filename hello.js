1e3c2abc90b5fa1cea68cd6c930d34026419baa1
RajanMaharjan <razanmaharjan@gmail.com>
2015-04-03 12:54:17 +0545

    My bit bucket comment :)

--- hello.js
+++ hello.js
@@ -5,12 +5,25 @@ if (Meteor.isClient) {
     // This code only runs on the client
     Template.body.helpers({
         tasks: function () {
-            // Show newest tasks first
-            return Tasks.find({}, {sort: {createdAt: -1}});        }
+            if (Session.get("hideCompleted")) {
+                // If hide completed is checked, filter tasks
+                return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+            } else {
+                // Otherwise, return all of the tasks
+                return Tasks.find({}, {sort: {createdAt: -1}});
+            }
+        },
+        hideCompleted: function () {
+            return Session.get("hideCompleted");
+        },
+        incompleteCount: function () {
+            return Tasks.find({checked: {$ne: true}}).count();
+        }
     });
 
     // Inside the if (Meteor.isClient) block, right after Template.body.helpers:
     Template.body.events({
+
         "submit .new-task": function (event) {
             // This function is called when the new task form is submitted
 
@@ -18,16 +31,25 @@ if (Meteor.isClient) {
 
             Tasks.insert({
                 text: text,
-                createdAt: new Date() // current time
+                createdAt: new Date(),            // current time
+                owner: Meteor.userId(),           // _id of logged in user
+                username: Meteor.user().username  // username of logged in user
             });
-
             // Clear form
             event.target.text.value = "";
 
             // Prevent default form submit
             return false;
+        },
+        // Add to Template.body.events
+        "change .hide-completed input": function (event) {
+            Session.set("hideCompleted", event.target.checked);
         }
+
+
     });
+
+
 
     // In the client code, below everything else
     Template.task.events({
@@ -38,5 +60,10 @@ if (Meteor.isClient) {
         "click .delete": function () {
             Tasks.remove(this._id);
         }
+    });
+
+    // At the bottom of the client code
+    Accounts.ui.config({
+        passwordSignupFields: "USERNAME_ONLY"
     });
 }
