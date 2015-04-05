5d5e00301a592454a8643510d66484572bce2137
RajanMaharjan <razanmaharjan@gmail.com>
2015-04-06 00:19:01 +0545

    My bit bucket comment :)

--- hello.js
+++ hello.js
@@ -1,16 +1,9 @@
-// my_bucket_list.js
 Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
-
-    // Define a helper to check if the current user is the task owner
-    Template.task.helpers({
-        isOwner: function () {
-            return this.owner === Meteor.userId();
-        }
-    });
-
     // This code only runs on the client
+    Meteor.subscribe("tasks");
+
     Template.body.helpers({
         tasks: function () {
             if (Session.get("hideCompleted")) {
@@ -29,22 +22,12 @@ if (Meteor.isClient) {
         }
     });
 
-    // Inside the if (Meteor.isClient) block, right after Template.body.helpers:
     Template.body.events({
-
         "submit .new-task": function (event) {
             // This function is called when the new task form is submitted
-
             var text = event.target.text.value;
 
-            // replace Tasks.insert( ... ) with:
             Meteor.call("addTask", text);
-
-            // replace Tasks.update( ... ) with:
-            Meteor.call("setChecked", this._id, ! this.checked);
-
-            // replace Tasks.remove( ... ) with:
-            Meteor.call("deleteTask", this._id);
 
             // Clear form
             event.target.text.value = "";
@@ -52,43 +35,39 @@ if (Meteor.isClient) {
             // Prevent default form submit
             return false;
         },
-        // Add to Template.body.events
         "change .hide-completed input": function (event) {
             Session.set("hideCompleted", event.target.checked);
         }
-
-
     });
 
-
-
-    // In the client code, below everything else
     Template.task.events({
         "click .toggle-checked": function () {
             // Set the checked property to the opposite of its current value
-            Tasks.update(this._id, {$set: {checked: ! this.checked}});
+            Meteor.call("setChecked", this._id, ! this.checked);
         },
         "click .delete": function () {
-            Tasks.remove(this._id);
+            Meteor.call("deleteTask", this._id);
         },
-        // Add an event for the new button to Template.task.events
         "click .toggle-private": function () {
             Meteor.call("setPrivate", this._id, ! this.private);
         }
-
     });
 
-    // At the bottom of the client code
+    Template.task.helpers({
+        isOwner: function () {
+            return this.owner === Meteor.userId();
+        }
+    });
+
     Accounts.ui.config({
         passwordSignupFields: "USERNAME_ONLY"
     });
 }
 
-// At the bottom of simple-todos.js, outside of the client-only block
 Meteor.methods({
     addTask: function (text) {
         // Make sure the user is logged in before inserting a task
-        if (task.owner !== Meteor.userId()) {
+        if (! Meteor.userId()) {
             throw new Meteor.Error("not-authorized");
         }
 
@@ -100,12 +79,12 @@ Meteor.methods({
         });
     },
     deleteTask: function (taskId) {
-        // Inside the deleteTask method
         var task = Tasks.findOne(taskId);
         if (task.private && task.owner !== Meteor.userId()) {
             // If the task is private, make sure only the owner can delete it
             throw new Meteor.Error("not-authorized");
         }
+
         Tasks.remove(taskId);
     },
     setChecked: function (taskId, setChecked) {
@@ -114,11 +93,12 @@ Meteor.methods({
             // If the task is private, make sure only the owner can check it off
             throw new Meteor.Error("not-authorized");
         }
+
         Tasks.update(taskId, { $set: { checked: setChecked} });
     },
     setPrivate: function (taskId, setToPrivate) {
-        var task;
-        task = Tasks.findOne(taskId);
+        var task = Tasks.findOne(taskId);
+
         // Make sure only the task owner can make a task private
         if (task.owner !== Meteor.userId()) {
             throw new Meteor.Error("not-authorized");
@@ -126,13 +106,10 @@ Meteor.methods({
 
         Tasks.update(taskId, { $set: { private: setToPrivate } });
     }
-
 });
 
-// At the bottom of simple-todos.js
 if (Meteor.isServer) {
-// Modify the publish statement
-// Only publish tasks that are public or belong to the current user
+    // Only publish tasks that are public or belong to the current user
     Meteor.publish("tasks", function () {
         return Tasks.find({
             $or: [
